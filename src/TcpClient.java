ed6a35f10de54adc0f154f1ecd836dcc0750bad3
coopervgarrison <cooper.garrison@vanderbilt.edu>
2017-10-17 00:45:55 -0500

    Initial commit

---
+++ src/TcpClient.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.net.*;
+
+public class TcpClient {
+    private static final String HOST_ADDRESS = "127.0.0.1";
+    private static final int PORT = 6789;
+
+    public static void main(String[] args) {
+        try {
+            boolean task = true;
+            String responseFromServer, userInput;
+
+            while (task) {
+                BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+                Socket socket = new Socket(HOST_ADDRESS, PORT);
+                DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
+                BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                responseFromServer = inFromServer.readLine();
+                if (responseFromServer.equalsIgnoreCase("Waiting for a command.")
+                        || responseFromServer.equalsIgnoreCase("Please enter key.")
+                        || responseFromServer.equalsIgnoreCase("Please enter value.")
+                        || responseFromServer.equalsIgnoreCase("Please enter key to retrieve its value.")) {
+                    System.out.println(responseFromServer);
+                    userInput = inFromUser.readLine();
+                    outToServer.writeBytes(userInput + "\n");
+                }
+                else {
+                    System.out.println(responseFromServer);
+                    task = false;
+                    socket.close();
+                }
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/TcpServer.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class TcpServer {
+    private static final int PORT = 6789;
+
+    public static void main(String argv[]) {
+        try {
+            String clientSentence = "", key = "", value = "";
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            Map<String, String> map = new HashMap<String, String>();
+
+            //while (true) {
+                Socket connectionSocket = serverSocket.accept();
+                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+                outToClient.writeBytes("Waiting for a command.\n");
+                clientSentence = inFromClient.readLine();
+                if (clientSentence.equalsIgnoreCase("store")) {
+                    outToClient.writeBytes("Please enter key.\n");
+                    key = inFromClient.readLine();
+                    outToClient.writeBytes("Please enter value.\n");
+                    value = inFromClient.readLine();
+                    map.put(key, value);
+                    outToClient.writeBytes("Successfully stored : " + key + ": " + value + "\n");
+                }
+                else if (clientSentence.equalsIgnoreCase("retrieve")) {
+                    outToClient.writeBytes("Please enter key to retrieve its value.\n");
+                    key = inFromClient.readLine();
+                    value = map.containsKey(key) ? "Value: " + map.get(key) + "\n" : "Invalid Key!\n";
+                    outToClient.writeBytes(value);
+                }
+                else {
+                    outToClient.writeBytes("Invalid Command.\n");
+                }
+            //}
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
